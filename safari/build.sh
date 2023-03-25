41ebe47c99ea0c55bd58d97ec7db4a2ad24c7323
josc146 <josStorer@outlook.com>
2023-03-25 09:48:02 +0800

    build: adjust to comply with apple's review policy

--- safari/build.sh
+++ safari/build.sh
@@ -1,10 +1,12 @@
+git apply safari/project.pre.patch
+npm run build
 xcrun safari-web-extension-converter ./build/firefox \
- --project-location ./build/safari --app-name chatGPTBox \
+ --project-location ./build/safari --app-name "Fission - ChatBox" \
  --bundle-identifier dev.josStorer.chatGPTBox --force --no-prompt --no-open
 git apply safari/project.patch
-xcodebuild archive -project ./build/safari/chatGPTBox/chatGPTBox.xcodeproj \
- -scheme "chatGPTBox (macOS)" -configuration Release -archivePath ./build/safari/chatGPTBox.xcarchive
-xcodebuild -exportArchive -archivePath ./build/safari/chatGPTBox.xcarchive \
+xcodebuild archive -project "./build/safari/Fission - ChatBox/Fission - ChatBox.xcodeproj" \
+ -scheme "Fission - ChatBox (macOS)" -configuration Release -archivePath "./build/safari/Fission - ChatBox.xcarchive"
+xcodebuild -exportArchive -archivePath "./build/safari/Fission - ChatBox.xcarchive" \
  -exportOptionsPlist ./safari/export-options.plist -exportPath ./build
 npm install -D appdmg
 rm ./build/safari.dmg

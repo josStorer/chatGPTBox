fc982e43a417d3c07ebc3de1a42dd82b7e720a2f
josc146 <josStorer@outlook.com>
2023-03-31 20:04:38 +0800

    feat: option to regenerate the answer after switching model (#98)

---
+++ src/hooks/use-config.mjs
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { defaultConfig, getUserConfig } from '../config/index.mjs'
+import Browser from 'webextension-polyfill'
+
+export function useConfig(initFn) {
+  const [config, setConfig] = useState(defaultConfig)
+  useEffect(() => {
+    getUserConfig().then((config) => {
+      setConfig(config)
+      if (initFn) initFn()
+    })
+  }, [])
+  useEffect(() => {
+    const listener = (changes) => {
+      const changedItems = Object.keys(changes)
+      let newConfig = {}
+      for (const key of changedItems) {
+        newConfig[key] = changes[key].newValue
+      }
+      setConfig({ ...config, ...newConfig })
+    }
+    Browser.storage.local.onChanged.addListener(listener)
+    return () => {
+      Browser.storage.local.onChanged.removeListener(listener)
+    }
+  }, [config])
+  return config
+}

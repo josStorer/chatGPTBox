ab95a3edcd3f2b0669ce87a5233777a4de632ecb
josc146 <josStorer@outlook.com>
2023-04-05 14:56:23 +0800

    refactor: setAbortController

--- src/background/apis/shared.mjs
+++ src/background/apis/shared.mjs
@@ -1,3 +1,27 @@
+export function setAbortController(port, onStop, onDisconnect) {
+  const controller = new AbortController()
+  const messageListener = (msg) => {
+    if (msg.stop) {
+      port.onMessage.removeListener(messageListener)
+      console.debug('stop generating')
+      port.postMessage({ done: true })
+      controller.abort()
+      if (onStop) onStop()
+    }
+  }
+  port.onMessage.addListener(messageListener)
+
+  const disconnectListener = () => {
+    port.onDisconnect.removeListener(disconnectListener)
+    console.debug('port disconnected')
+    controller.abort()
+    if (onDisconnect) onDisconnect()
+  }
+  port.onDisconnect.addListener(disconnectListener)
+
+  return { controller, messageListener }
+}
+
 export function pushRecord(session, question, answer) {
   const recordLength = session.conversationRecords.length
   let lastRecord

64fe14db796415502f3ff854df130751f1b6e14d
josc146 <josStorer@outlook.com>
2023-04-05 15:31:55 +0800

    refactor: getConversationPairs and promptBase

--- src/background/apis/shared.mjs
+++ src/background/apis/shared.mjs
@@ -1,3 +1,22 @@
+export const getChatSystemPromptBase = async () => {
+  return `You are a helpful, creative, clever, and very friendly assistant. You are familiar with various languages in the world.`
+}
+
+export const getCompletionPromptBase = async () => {
+  return (
+    `The following is a conversation with an AI assistant.` +
+    `The assistant is helpful, creative, clever, and very friendly. The assistant is familiar with various languages in the world.\n\n` +
+    `Human: Hello, who are you?\n` +
+    `AI: I am an AI assistant. How can I help you today?\n` +
+    `Human: 没什么\n` +
+    `AI: 好的, 如果有什么需要, 随时告诉我\n`
+  )
+}
+
+export const getCustomApiPromptBase = async () => {
+  return `I am a helpful, creative, clever, and very friendly assistant. I am familiar with various languages in the world.`
+}
+
 export function setAbortController(port, onStop, onDisconnect) {
   const controller = new AbortController()
   const messageListener = (msg) => {

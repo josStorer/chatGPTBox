fe5081d9ab2d2047145621088cc2ae7a707b84bb
josc146 <josStorer@outlook.com>
2023-04-08 20:18:38 +0800

    chore: better `openUrl` for independent conversation page

--- src/utils/open-url.mjs
+++ src/utils/open-url.mjs
@@ -1,7 +1,7 @@
 import Browser from 'webextension-polyfill'
 
 export function openUrl(url) {
-  Browser.tabs.query({ url }).then((tabs) => {
+  Browser.tabs.query({ url, currentWindow: true }).then((tabs) => {
     if (tabs.length > 0) {
       Browser.tabs.update(tabs[0].id, { active: true })
     } else {

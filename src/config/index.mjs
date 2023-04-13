240e19f3bca6723da8aab1628c435a60a3e7dd01
josc146 <josStorer@outlook.com>
2023-04-13 17:01:29 +0800

    feat: use reliable reverse proxy by default `https://bypass.churchless.tech/api/conversation` @acheong08

--- src/config/index.mjs
+++ src/config/index.mjs
@@ -85,8 +85,8 @@ export const defaultConfig = {
   // advanced
 
   maxResponseTokenLength: 1000,
-  customChatGptWebApiUrl: 'https://chat.openai.com',
-  customChatGptWebApiPath: '/backend-api/conversation',
+  customChatGptWebApiUrl: 'https://bypass.churchless.tech', // https://chat.openai.com
+  customChatGptWebApiPath: '/api/conversation', // /backend-api/conversation
   customOpenAiApiUrl: 'https://api.openai.com',
   siteRegex: 'match nothing',
   useSiteRegexOnly: false,

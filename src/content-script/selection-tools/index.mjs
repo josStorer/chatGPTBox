8d2995970fd14f5ae5b79a93d1837265ed3c8792
AiraNadih <128119996+AiraNadih@users.noreply.github.com>
2023-11-16 04:05:53 +0800

    Refactor genPrompt functions and improve code readability

--- src/content-script/selection-tools/index.mjs
+++ src/content-script/selection-tools/index.mjs
@@ -11,91 +11,95 @@ import {
 } from 'react-bootstrap-icons'
 import { getPreferredLanguage } from '../../config/language.mjs'
 
+const createGenPrompt =
+  (message, isTranslation = false, targetLanguage = '') =>
+  async (selection) => {
+    const preferredLanguage = isTranslation
+      ? targetLanguage
+      : await getPreferredLanguage()
+    return `Reply in ${preferredLanguage}.${message}:\n'''\n${selection}\n'''`
+  }
+
 export const config = {
   explain: {
     icon: <ChatText />,
     label: 'Explain',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Reply in ${preferredLanguage}.Explain the following:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt('Explain the following'),
   },
   translate: {
     icon: <Translate />,
     label: 'Translate',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Translate the following into ${preferredLanguage} and only show me the translated content:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Translate the following into ${preferredLanguage} and only show me the translated content',
+      true
+    ),
   },
   translateToEn: {
     icon: <Globe />,
     label: 'Translate (To English)',
-    genPrompt: async (selection) => {
-      return `Translate the following into English and only show me the translated content:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Translate the following into English and only show me the translated content',
+      true,
+      'English'
+    ),
   },
   translateToZh: {
     icon: <Globe />,
     label: 'Translate (To Chinese)',
-    genPrompt: async (selection) => {
-      return `Translate the following into Chinese and only show me the translated content:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Translate the following into Chinese and only show me the translated content',
+      true,
+      'Chinese'
+    ),
   },
   translateBidi: {
     icon: <Globe />,
     label: 'Translate (Bidirectional)',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return (
-        `Translate the following into ${preferredLanguage} and only show me the translated content.` +
-        `If it is already in ${preferredLanguage},` +
-        `translate it into English and only show me the translated content:\n"""\n${selection}\n"""`
-      )
-    },
+    genPrompt: createGenPrompt(
+      'Translate the following into ${preferredLanguage} and only show me the translated content. If it is already in ${preferredLanguage}, translate it into English and only show me the translated content',
+      true
+    ),
   },
   summary: {
     icon: <CardHeading />,
     label: 'Summary',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Reply in ${preferredLanguage}.Summarize the following as concisely as possible:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Summarize the following as concisely as possible'
+    ),
   },
   polish: {
     icon: <Palette />,
     label: 'Polish',
-    genPrompt: async (selection) =>
-      `Check the following content for possible diction and grammar problems,and polish it carefully:\n"""\n${selection}\n"""`,
+    genPrompt: createGenPrompt(
+      'Check the following content for possible diction and grammar problems, and polish it carefully',
+      false
+    ),
   },
   sentiment: {
     icon: <EmojiSmile />,
     label: 'Sentiment Analysis',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Reply in ${preferredLanguage}.Analyze the sentiments expressed in the following content and make a brief summary of the sentiments:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Analyze the sentiments expressed in the following content and make a brief summary of the sentiments'
+    ),
   },
   divide: {
     icon: <CardList />,
     label: 'Divide Paragraphs',
-    genPrompt: async (selection) =>
-      `Divide the following into paragraphs that are easy to read and understand:\n"""\n${selection}\n"""`,
+    genPrompt: createGenPrompt(
+      'Divide the following into paragraphs that are easy to read and understand',
+      false
+    ),
   },
   code: {
     icon: <Braces />,
     label: 'Code Explain',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Reply in ${preferredLanguage}.Explain the following code:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt('Explain the following code'),
   },
   ask: {
     icon: <QuestionCircle />,
     label: 'Ask',
-    genPrompt: async (selection) => {
-      const preferredLanguage = await getPreferredLanguage()
-      return `Reply in ${preferredLanguage}.Analyze the following content and express your opinion,or give your answer:\n"""\n${selection}\n"""`
-    },
+    genPrompt: createGenPrompt(
+      'Analyze the following content and express your opinion, or give your answer'
+    ),
   },
 }
